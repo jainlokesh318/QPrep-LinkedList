9b7c8def8b1e0ece88e423f477ee8e240dea6b26
jainlokesh318 <jainlokesh318@gmail.com>
2020-05-05 18:46:46 +0000

    [Solved]:[RemoveDuplicatesFromList]:[c++14]

--- RemoveDuplicatesFromList/Solution.cpp
+++ RemoveDuplicatesFromList/Solution.cpp
@@ -15,4 +15,28 @@ public:
 */
 ListNode* removeDuplicatesFromList(ListNode* head)
 {
+     if(head == NULL || head->next == NULL)
+        return head;
+
+
+    ListNode *rep = new ListNode();
+    rep->next = head;
+    ListNode *prev = rep, *curr = head;
+
+    while(curr != NULL)
+    {
+        while(curr->next != NULL && (prev->next->val == curr->next->val))
+            curr = curr->next;
+
+        if(prev->next == curr)
+            prev = prev->next;
+        else
+            prev->next = curr->next;
+
+        curr = curr->next;
+    }
+
+    head = rep->next;
+    return head;
+
 }
