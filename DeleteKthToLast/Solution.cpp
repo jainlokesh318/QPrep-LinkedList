6b2f5e1d7c3652e2f2be1e6e307e0ca290d10729
jainlokesh318 <jainlokesh318@gmail.com>
2020-05-06 18:47:46 +0000

    [LocalTest]:[DeleteKthToLast]:[c++14]

--- DeleteKthToLast/Solution.cpp
+++ DeleteKthToLast/Solution.cpp
@@ -13,4 +13,30 @@ public:
 };
 */
 ListNode* deleteKthToLast(ListNode* head , int k) {
+    ListNode* temp = head;
+
+    int cnt = 0;
+    while(temp != NULL)
+    {
+        cnt++;
+        temp = temp->next;
+    }
+
+    if(cnt == k)
+        return head->next;
+    else
+    {
+        k = cnt-k;
+        temp = head;
+        ListNode * prev = NULL;
+        while(k--)
+        {
+            prev = temp;
+            temp = temp->next;
+        }
+
+        temp = temp->next;
+        prev->next = temp;
+        return head;
+    }
 }
