bbf71997a8a87687bf8e32a54bfd9df0f0236d1c
jainlokesh318 <jainlokesh318@gmail.com>
2020-05-06 18:51:02 +0000

    [LocalTest]:[MoveMiddleToHead]:[c++14]

--- MoveMiddleToHead/Solution.cpp
+++ MoveMiddleToHead/Solution.cpp
@@ -14,4 +14,30 @@ public:
 */
 
 ListNode * moveMiddleToHead(ListNode * head) {
+    if(head == NULL || head->next == NULL)
+        return head;
+
+    ListNode* temp = head;
+
+    int cnt = 0;
+    while(temp != NULL)
+    {
+        cnt++;
+        temp = temp->next;
+    }
+
+
+    int k = (cnt/2);
+    temp = head;
+    ListNode * prev = NULL;
+    while(k--)
+    {
+        prev = temp;
+        temp = temp->next;
+    }
+
+    prev->next = temp->next;
+    temp->next = head;
+    head = temp;
+    return head;
 }
