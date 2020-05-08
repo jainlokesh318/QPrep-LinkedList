67a9bc9e41c8cc6c8d7186db3f92865796214273
jainlokesh318 <jainlokesh318@gmail.com>
2020-05-08 18:44:22 +0000

    [LocalTest]:[ReverseEvenElements]:[c++14]

--- ReverseEvenElements/Solution.cpp
+++ ReverseEvenElements/Solution.cpp
@@ -13,6 +13,46 @@ public:
     }
 };
 */
+ListNode* reverseEvenSublist(ListNode* prev, ListNode* curr)
+{
+
+    ListNode* nextNode; //to keep track of the next node to current
+    ListNode* initial = curr; //Since this node will be tail after reversal which is to be pointed to the next sublist.
+    while(curr != NULL && curr->val%2 == 0)
+    {
+        nextNode = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = nextNode;
+    }
+
+    initial->next = curr;
+    return prev;
+}
 
 ListNode * reverseEvenElements(ListNode *head , ListNode *prev) {
+    if(head == NULL)
+        return NULL;
+    ListNode *curr = head;
+
+    while(curr != NULL)
+    {
+        if((curr->val)%2 != 0 || curr->next == NULL || (curr->next->val)%2 == 1)
+        {
+            prev = curr;
+            curr = curr->next;
+        }
+
+        else
+        {
+            if(curr == head)
+                head = reverseEvenSublist(prev, curr);
+            else
+            {
+               ListNode* OldPrev = prev;
+               OldPrev->next = reverseEvenSublist(prev, curr);
+            }
+        }
+    }
+    return head;
 }
