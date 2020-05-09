048dc398ff29fc0d3a8d4ebdf252f9293ebe2186
jainlokesh318 <jainlokesh318@gmail.com>
2020-05-09 07:20:21 +0000

    [LocalTest]:[Partition]:[c++14]

--- Partition/Solution.cpp
+++ Partition/Solution.cpp
@@ -14,5 +14,119 @@ public:
 };
 */
 
-ListNode* partition(ListNode* head , int x) {
+ListNode* partition(ListNode* head, int x)
+{
+    ListNode* temp = head, *prev = NULL;
+    ListNode* newNode = new ListNode();
+    ListNode* toSend = newNode;
+ //---------------------------------------------------------------------------------
+    while(temp != NULL)
+    {
+        if(temp->val < x)
+        {
+            if(prev == NULL)
+            {
+                head = temp->next;
+                temp->next = NULL;
+
+                newNode->next = temp;
+                newNode = newNode->next;
+
+                temp = head;
+            }
+
+            else
+            {
+                prev->next = temp->next;
+                temp->next = NULL;
+
+                newNode->next = temp;
+                newNode = newNode->next;
+
+                temp = prev->next;
+            }
+        }
+
+        else
+        {
+            prev = temp;
+            temp = temp->next;
+        }
+    }
+//-------------------------------------------------------------------------------------
+    temp = head;
+    prev = NULL;
+
+    while(temp != NULL)
+    {
+        if(temp->val == x)
+        {
+            if(prev == NULL)
+            {
+                head = temp->next;
+                temp->next = NULL;
+
+                newNode->next = temp;
+                newNode = newNode->next;
+
+                temp = head;
+            }
+
+            else
+            {
+                prev->next = temp->next;
+                temp->next = NULL;
+
+                newNode->next = temp;
+                newNode = newNode->next;
+
+                temp = prev->next;
+            }
+        }
+
+        else
+        {
+            prev = temp;
+            temp = temp->next;
+        }
+    }
+
+//-------------------------------------------------------------------------------------
+    temp = head;
+    prev = NULL;
+    while(temp != NULL)
+    {
+        if(temp->val > x)
+        {
+            if(prev == NULL)
+            {
+                head = temp->next;
+                temp->next = NULL;
+
+                newNode->next = temp;
+                newNode = newNode->next;
+
+                temp = head;
+            }
+
+            else
+            {
+                prev->next = temp->next;
+                temp->next = NULL;
+
+                newNode->next = temp;
+                newNode = newNode->next;
+
+                temp = prev->next;
+            }
+        }
+
+        else
+        {
+            prev = temp;
+            temp = temp->next;
+        }
+    }
+//-----------------------------------------------------------------------------------
+    return toSend->next;
 }
